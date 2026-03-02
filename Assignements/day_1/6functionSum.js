0bf05c348fb9aec4722d33bcac6776869ac6013b
Gajji Mahesh <gajjimahesh1092@gmail.com>
2026-03-02 18:49:53 +0530

    updated totalsum function and written output
    Added output comment for clarity.

--- Assignements/day_1/6functionSum.js
+++ Assignements/day_1/6functionSum.js
@@ -2,10 +2,13 @@
 function totalSum(numbers){
 let sum=0;
 for(let i=0;i<numbers.length;i++){
+    
     sum=sum+numbers[i];
 }
 return sum;
 }
 // calling the function totalSum and storing in result variable
 let result=totalSum([10,20,30]) 
+
  console.log(result)
+// output 60
